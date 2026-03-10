80ae8d283d8e60855f6864913317d08a70288948
bhavishb-24 <bhavishballani@gmail.com>
2026-03-09 21:32:59 -0500

    Add user input sanitization

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -9,6 +9,14 @@ and prints the result to the terminal window.
 
 """
 
+def request_santized_number(prompt: str) -> float:
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
     Function that takes in two numbers and an operation (add, subtract, multiply, divide),
